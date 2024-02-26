0e3bab75fa3c7b34c8c818198813f3e5131d8f18
Sean <seanphilip419@gmail.com>
2024-02-26 10:37:33 +0300

    models

--- mit/class/models.py
+++ mit/class/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    phone = models.IntegerField(blank=False)
+    email = models.EmailField(blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    phone = models.IntegerField(blank=False)
+    email = models.EmailField(blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class School(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    location = models.CharField(max_length=50, blank=False)
+    address = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.name
